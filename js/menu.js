4c4f6311f2d427dbaa5ce475b91278d953176a90
Javier <eljavimolines@gmail.com>
2019-09-29 21:59:35 -0400

    Pagina Ajedrez
    Todos los cambios ya guardados en mi pc

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+function cerrar_menu() {
+    
+  var estado = document.getElementById('menu_desplegar');
+  
+  estado.style.display = 'none';
+  
+}
+
+function abrir_menu() {
+    
+  var estado = document.getElementById('menu_desplegar');
+
+  estado.style.display = 'block';
+  
+}
